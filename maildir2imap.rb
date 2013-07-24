a7eff396b6e66759d8cf9c18ab71bca41942d4be
Faishal <saiyedfaishal@gmail.com>
2013-07-24 22:29:36 +0530

    format code

--- maildir2imap.rb
+++ maildir2imap.rb
@@ -6,10 +6,8 @@ imap_dir=ARGV[4]
 
 require 'maildir'
 require 'net/imap'
-#require 'mail'
 require 'time'
 #Create object
- 
 puts  "maildir"
 maildir = Maildir.new(mail_dir, false); #avoid creating new maildir if not exist
 #maildir.serializer = Maildir::Serializer::Mail.new
@@ -20,23 +18,16 @@ puts imap.login(imap_user, imap_pass)
 puts "auth done"
 puts imap.examine(imap_dir)
 maildir.list(:new).each do |singlemail|
-puts maildir.list(:new).first.data
-
-	
-date = singlemail.data.scan(/Delivery-date:(.*[^-a-zA-Z1-9_.])?/i)
-puts date
-
-puts "DATE IS HERE"
-
-#puts mail.date.to_s
-
-puts "add message\r\n"
-#ctime =  Time.parse(mail.date.to_s)
-ctime = Time.parse(date.to_s)
-puts ctime
- imap.append(imap_dir,singlemail.data,nil,ctime)
-puts "mesage added"
+	puts maildir.list(:new).first.data
+	date = singlemail.data.scan(/Delivery-date:(.*[^-a-zA-Z1-9_.])?/i)
+	puts date
+	puts "DATE IS HERE"
+	#puts mail.date.to_s
+	puts "add message\r\n"
+	#ctime =  Time.parse(mail.date.to_s)
+	ctime = Time.parse(date.to_s)
+	puts ctime
+ 	imap.append(imap_dir,singlemail.data,nil,ctime)
+	puts "mesage added"
 end
 puts "done"
-exit
-
