9f725535de0ec7f0750f29bcd2cf90f71cc61182
root <root@jagoinvestor.com>
2013-07-25 03:01:29 -0400

    Added mailbox creation if not exist
    ter: root <root@jagoinvestor.com>

--- maildir2imap.rb
+++ maildir2imap.rb
@@ -4,6 +4,11 @@ imap_user=ARGV[2]
 imap_pass=ARGV[3]
 imap_dir=ARGV[4]
 
+puts mail_dir
+puts imap_host
+puts imap_user
+puts imap_pass
+puts imap_dir
 require 'maildir'
 require 'net/imap'
 #require 'mail'
@@ -18,25 +23,29 @@ imap = Net::IMAP.new('imap.gmail.com',993,true)
 puts "before auth"
 puts imap.login(imap_user, imap_pass)
 puts "auth done"
-puts imap.examine(imap_dir)
-maildir.list(:new).each do |singlemail|
-puts maildir.list(:new).first.data
 
-	
-date = singlemail.data.scan(/Delivery-date:(.*[^-a-zA-Z1-9_.])?/i)
-puts date
-
-puts "DATE IS HERE"
-
-#puts mail.date.to_s
-
-puts "add message\r\n"
-#ctime =  Time.parse(mail.date.to_s)
-ctime = Time.parse(date.to_s)
-puts ctime
- imap.append(imap_dir,singlemail.data,nil,ctime)
-puts "mesage added"
+begin
+	puts imap.examine(imap_dir)
+rescue
+	puts imap.create(imap_dir)
+ensure
+	puts imap.examine(imap_dir)
 end
+
+maildir.list(:new).each do |singlemail|
+	puts maildir.list(:new).first.data
+	date = singlemail.data.scan(/Delivery-date:(.*[^-a-zA-Z1-9_.])?/i)
+	puts date
+
+	puts "DATE IS HERE"
+
+	puts "add message\r\n"
+	ctime = Time.parse(date.to_s)
+	puts ctime
+	imap.append(imap_dir,singlemail.data,nil,ctime)
+	puts "mesage added"
+end
+	
 puts "done"
 exit
 
