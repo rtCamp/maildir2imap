9f9321d3bfd56679cdb68983ccf201de99aa8081
Rahul Bansal <rahul.bansal@rtcamp.com>
2013-07-25 04:23:45 -0400

    Loop progress indicator

--- maildir2imap.rb
+++ maildir2imap.rb
@@ -21,31 +21,37 @@ maildir = Maildir.new(mail_dir, false); #avoid creating new maildir if not exist
 puts "before IMAP"
 imap = Net::IMAP.new('imap.gmail.com',993,true)
 puts "before auth"
-puts imap.login(imap_user, imap_pass)
+imap.login(imap_user, imap_pass)
 puts "auth done"
 
 begin
-	puts imap.examine(imap_dir)
+	imap.examine(imap_dir)
 rescue
-	puts imap.create(imap_dir)
+	imap.create(imap_dir)
 ensure
-	puts imap.examine(imap_dir)
+	imap.examine(imap_dir)
 end
 
-maildir.list(:new).each do |singlemail|
-	puts maildir.list(:new).first.data
-	date = singlemail.data.scan(/Delivery-date:(.*[^-a-zA-Z1-9_.])?/i)
-	puts date
-
-	puts "DATE IS HERE"
-
-	puts "add message\r\n"
-	ctime = Time.parse(date.to_s)
-	puts ctime
+puts "moving cur"
+maildir.list(:cur).each_with_index do |singlemail, index|
+	#puts maildir.list(:new).first.data
+	date = singlemail.data.scan(/(Delivery-date|Date):(.*[^-a-zA-Z1-9_.])?/i)
+	print date.inspect
+	ctime = Time.parse(date[1].to_s)
+#	puts ctime
 	imap.append(imap_dir,singlemail.data,nil,ctime)
-	puts "mesage added"
+	puts "mesage added #{index+1} / #{maildir.list(:cur).size}"
 end
-	
+
+puts "moving new"
+maildir.list(:new).each_with_index do |singlemail, index|
+        #puts maildir.list(:new).first.data
+        date = singlemail.data.scan(/Delivery-date:(.*[^-a-zA-Z1-9_.])?/i)
+        ctime = Time.parse(date.to_s)
+ #       puts ctime
+        imap.append(imap_dir,singlemail.data,nil,ctime)
+        puts "mesage added #{index+1} / #{maildir.list(:new).size}"
+end	
 puts "done"
 exit
 
